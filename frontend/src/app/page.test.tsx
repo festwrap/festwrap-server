86557a3ec9774dd65db7dfae99a3b767a35eef9d
alex.barro <alex.barro@zinklar.com>
2024-08-25 10:53:27 +0200

    test: add home page cases

---
+++ frontend/src/app/page.test.tsx
@@ -0,0 +1,51 @@
+import { describe, test, expect, vi, Mock } from "vitest"
+import { render } from "@testing-library/react"
+import Home from "./page"
+import { useSession } from "next-auth/react"
+
+vi.mock("next-auth/react", () => {
+  const mockSession = {
+    expires: new Date(Date.now() + 2 * 86400).toISOString(),
+    user: { username: "admin" },
+  }
+  return {
+    __esModule: true,
+    useSession: vi.fn(),
+  }
+})
+
+const useSessionMock = useSession as Mock<typeof useSession>
+
+describe("Home", () => {
+  test("should render sign in button when there is not session", () => {
+    const mockSession = {
+      expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      user: { username: "user", email: "user@gmail.com" },
+    }
+
+    useSessionMock.mockReturnValue({
+      update: vi.fn(),
+      data: null,
+      status: "unauthenticated",
+    })
+
+    const { getByText } = render(<Home />)
+    expect(getByText("Not signed in")).toBeTruthy()
+  })
+
+  test("should render sign out button when there is session", () => {
+    const mockSession = {
+      expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      user: { username: "user", email: "user@gmail.com" },
+    }
+
+    useSessionMock.mockReturnValue({
+      update: vi.fn(),
+      data: mockSession,
+      status: "authenticated",
+    })
+
+    const { getByText } = render(<Home />)
+    expect(getByText("Not signed in")).toBeTruthy()
+  })
+})
