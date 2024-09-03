5ca92ceb37254e2c0493006928969c5da06103a0
Dani <daniel.mora.checa@gmail.com>
2024-09-03 09:51:11 +0200

    refactor(songs): use generic deserialize interface

---
+++ backend/internal/song/spotify/spotify_songs_deserializer.go
@@ -0,0 +1,28 @@
+package spotify
+
+import (
+	"encoding/json"
+
+	"festwrap/internal/serialization/errors"
+	"festwrap/internal/song"
+)
+
+type SpotifySongsDeserializer struct{}
+
+func (s *SpotifySongsDeserializer) Deserialize(setlist []byte) (*[]song.Song, error) {
+	var response SpotifyResponse
+	err := json.Unmarshal(setlist, &response)
+	if err != nil {
+		return nil, errors.NewDeserializationError(err.Error())
+	}
+
+	result := []song.Song{}
+	for _, currentSong := range response.Tracks.Songs {
+		result = append(result, song.NewSong(currentSong.Uri))
+	}
+	return &result, nil
+}
+
+func NewSpotifySongsDeserializer() SpotifySongsDeserializer {
+	return SpotifySongsDeserializer{}
+}
