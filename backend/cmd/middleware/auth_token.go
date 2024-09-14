149163bcf8114847a4a2fbf3bad938b2e8bcf478
Dani <daniel.mora.checa@gmail.com>
2024-09-14 07:20:11 +0200

    feat(middleware): add auth token middleware

---
+++ backend/cmd/middleware/auth_token.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	types "festwrap/internal"
+)
+
+// Extracts the Bearer Auth token in the header and stores in the context variable with the given key
+type AuthTokenMiddleware struct {
+	tokenKey types.ContextKey
+	handler  http.Handler
+}
+
+func NewAuthTokenMiddleware(tokenKey types.ContextKey, handler http.Handler) AuthTokenMiddleware {
+	return AuthTokenMiddleware{tokenKey: tokenKey, handler: handler}
+}
+
+func (m AuthTokenMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		http.Error(w, "Missing authorization header", http.StatusBadRequest)
+		return
+	} else if !strings.HasPrefix(authHeader, "Bearer ") {
+		http.Error(w, "Unexpected authorization header format", http.StatusUnprocessableEntity)
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	ctxWithToken := context.WithValue(r.Context(), m.tokenKey, token)
+	requestWithToken := r.WithContext(ctxWithToken)
+	m.handler.ServeHTTP(w, requestWithToken)
+}
