c03d17a03f1cb28a57234a339009732209cbf2ac
alex.barro <alex.barro@zinklar.com>
2024-09-15 12:21:31 +0200

    feat: use layout components in root layout

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
+import Header from "@components/Header"
+import Footer from "@components/Footer"
+import Main from "@components/Main"
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex flex-col h-full items-center justify-center bg-gray-50">
-      {children}
+    <div className="flex flex-col">
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
     </div>
   )
 }
