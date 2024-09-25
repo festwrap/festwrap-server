6a490a7a0c409309d701ce07b85ce70f815ccc3a
Dani <daniel.mora.checa@gmail.com>
2024-09-25 09:25:58 +0200

    chore: add get args in FakeSerializer

--- internal/serialization/fake_serializer.go
+++ internal/serialization/fake_serializer.go
@@ -3,6 +3,7 @@ package serialization
 type FakeSerializer[T any] struct {
 	response []byte
 	err      error
+	args     T
 }
 
 func (s *FakeSerializer[T]) SetResponse(response []byte) {
@@ -13,7 +14,12 @@ func (s *FakeSerializer[T]) SetError(err error) {
 	s.err = err
 }
 
+func (s *FakeSerializer[T]) GetArgs() T {
+	return s.args
+}
+
 func (s *FakeSerializer[T]) Serialize(input T) ([]byte, error) {
+	s.args = input
 	if s.err != nil {
 		return nil, s.err
 	}
