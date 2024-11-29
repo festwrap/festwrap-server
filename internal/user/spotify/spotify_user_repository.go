a06b7ade5805343fd40f7e611caa1978b4b9b26f
Dani <daniel.mora.checa@gmail.com>
2024-11-29 14:00:15 +0100

    feat: implement user id middleware

---
+++ internal/user/spotify/spotify_user_repository.go
@@ -0,0 +1,68 @@
+package spotify
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	types "festwrap/internal"
+	httpsender "festwrap/internal/http/sender"
+	"festwrap/internal/serialization"
+)
+
+type SpotifyUserRepository struct {
+	tokenKey     types.ContextKey
+	deserializer serialization.Deserializer[spotifyUserResponse]
+	httpSender   httpsender.HTTPRequestSender
+	host         string
+}
+
+func NewSpotifyUserRepository(httpSender httpsender.HTTPRequestSender) SpotifyUserRepository {
+	return SpotifyUserRepository{
+		tokenKey:     "token",
+		deserializer: serialization.NewJsonDeserializer[spotifyUserResponse](),
+		httpSender:   httpSender,
+		host:         "api.spotify.com",
+	}
+}
+
+func (r SpotifyUserRepository) GetCurrentUserId(ctx context.Context) (string, error) {
+	token, ok := ctx.Value(r.tokenKey).(string)
+	if !ok {
+		return "", errors.New("could not retrieve token from context")
+	}
+
+	responseBody, err := r.httpSender.Send(r.getCurrentUserIdHTTPOptions(token))
+	if err != nil {
+		return "", fmt.Errorf("could not get current user: %v", err.Error())
+	}
+
+	var response spotifyUserResponse
+	err = r.deserializer.Deserialize(*responseBody, &response)
+	if err != nil {
+		return "", fmt.Errorf("deserialization error: %v", err.Error())
+	}
+
+	return response.UserId, nil
+}
+
+func (r SpotifyUserRepository) getCurrentUserIdHTTPOptions(accessToken string) httpsender.HTTPRequestOptions {
+	url := fmt.Sprintf("https://%s/v1/me", r.host)
+	httpOptions := httpsender.NewHTTPRequestOptions(url, httpsender.GET, 200)
+	httpOptions.SetHeaders(
+		map[string]string{"Authorization": fmt.Sprintf("Bearer %s", accessToken)},
+	)
+	return httpOptions
+}
+
+func (r *SpotifyUserRepository) GetDeserializer() serialization.Deserializer[spotifyUserResponse] {
+	return r.deserializer
+}
+
+func (r *SpotifyUserRepository) SetDeserializer(deserializer serialization.Deserializer[spotifyUserResponse]) {
+	r.deserializer = deserializer
+}
+
+func (r *SpotifyUserRepository) SetTokenKey(key types.ContextKey) {
+	r.tokenKey = key
+}
