a00cdc1362bb456440dcb8cefb5bc7262d38134d
Dani <daniel.mora.checa@gmail.com>
2025-03-02 12:56:31 +0100

    chore: add new playlist update builder

--- internal/playlist/update_builders/playlist_update_builders.go
+++ internal/playlist/update_builders/playlist_update_builders.go
@@ -16,13 +16,18 @@ type PlaylistUpdateBuilder interface {
 
 type ExistingPlaylistUpdateBuilder struct {
 	pathId       string
-	deserializer serialization.Deserializer[PlaylistArtists]
+	deserializer serialization.Deserializer[ExistingPlaylistUpdate]
+}
+
+type NewPlaylistUpdateBuilder struct {
+	playlistService playlist.PlaylistService
+	deserializer    serialization.Deserializer[NewPlaylistUpdate]
 }
 
 func NewExistingPlaylistUpdateBuilder(pathId string) ExistingPlaylistUpdateBuilder {
 	return ExistingPlaylistUpdateBuilder{
 		pathId:       pathId,
-		deserializer: serialization.NewJsonDeserializer[PlaylistArtists](),
+		deserializer: serialization.NewJsonDeserializer[ExistingPlaylistUpdate](),
 	}
 }
 
@@ -38,7 +43,7 @@ func (b *ExistingPlaylistUpdateBuilder) Build(request *http.Request) (playlist.P
 		return playlist.PlaylistUpdate{}, errors.New("could read body from request")
 	}
 
-	var artists PlaylistArtists
+	var artists ExistingPlaylistUpdate
 	err = b.deserializer.Deserialize(requestBody, &artists)
 	if err != nil {
 		return playlist.PlaylistUpdate{}, errors.New("failed to deserialize playlist artists: " + err.Error())
@@ -52,6 +57,45 @@ func (b *ExistingPlaylistUpdateBuilder) Build(request *http.Request) (playlist.P
 	return update, nil
 }
 
-func (b *ExistingPlaylistUpdateBuilder) SetDeserializer(deserializer serialization.Deserializer[PlaylistArtists]) {
+func (b *ExistingPlaylistUpdateBuilder) SetDeserializer(deserializer serialization.Deserializer[ExistingPlaylistUpdate]) {
+	b.deserializer = deserializer
+}
+
+func NewNewPlaylistUpdateBuilder(playlistService playlist.PlaylistService) NewPlaylistUpdateBuilder {
+	return NewPlaylistUpdateBuilder{
+		playlistService: playlistService,
+		deserializer:    serialization.NewJsonDeserializer[NewPlaylistUpdate](),
+	}
+}
+
+func (b *NewPlaylistUpdateBuilder) Build(request *http.Request) (playlist.PlaylistUpdate, error) {
+	defer request.Body.Close()
+	requestBody, err := io.ReadAll(request.Body)
+	if err != nil {
+		return playlist.PlaylistUpdate{}, errors.New("could read body from request")
+	}
+
+	var update NewPlaylistUpdate
+	err = b.deserializer.Deserialize(requestBody, &update)
+	if err != nil {
+		return playlist.PlaylistUpdate{}, errors.New("failed to deserialize playlist information: " + err.Error())
+	}
+
+	playlistId, err := b.playlistService.CreatePlaylist(
+		request.Context(),
+		update.Playlist.toPlaylist(),
+	)
+	if err != nil {
+		return playlist.PlaylistUpdate{}, errors.New("could not create playlist")
+	}
+
+	playlistArtists := make([]playlist.PlaylistArtist, len(update.Artists))
+	for i, artist := range update.Artists {
+		playlistArtists[i] = playlist.PlaylistArtist{Name: artist.Name}
+	}
+	return playlist.PlaylistUpdate{PlaylistId: playlistId, Artists: playlistArtists}, nil
+}
+
+func (b *NewPlaylistUpdateBuilder) SetDeserializer(deserializer serialization.Deserializer[NewPlaylistUpdate]) {
 	b.deserializer = deserializer
 }
