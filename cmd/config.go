132b7c6031857d25ea434189d525d92bf672d6e6
Dani Mora <71647753+DanielMoraDC@users.noreply.github.com>
2025-05-30 08:11:45 +0200

    chore: use more concise name for env variable (#71)

--- cmd/config.go
+++ cmd/config.go
@@ -9,22 +9,22 @@ import (
 type Config struct {
 	Port                       string
 	MaxConnsPerHost            int
-	TimeoutSeconds             int
 	SetlistfmApiKey            string
 	MaxSetlistFMNumSearchPages int
 	MaxUpdateArtists           int
 	AddSetlistSleepMs          int
+	HttpClientTimeoutSeconds   int
 }
 
 func ReadConfig() Config {
 	return Config{
 		Port:                       GetEnvWithDefaultOrFail[string]("FESTWRAP_PORT", "8080"),
 		MaxConnsPerHost:            GetEnvWithDefaultOrFail[int]("FESTWRAP_MAX_CONNS_PER_HOST", 10),
-		TimeoutSeconds:             GetEnvWithDefaultOrFail[int]("FESTWRAP_TIMEOUT_SECONDS", 5),
 		SetlistfmApiKey:            GetEnvStringOrFail("FESTWRAP_SETLISTFM_APIKEY"),
 		MaxSetlistFMNumSearchPages: GetEnvWithDefaultOrFail[int]("FESTWRAP_SETLISTFM_NUM_SEARCH_PAGES", 3),
 		MaxUpdateArtists:           GetEnvWithDefaultOrFail[int]("FESTWRAP_MAX_UPDATE_ARTISTS", 5),
 		AddSetlistSleepMs:          GetEnvWithDefaultOrFail[int]("FESTWRAP_ADD_SETLIST_SLEEP_MS", 550),
+		HttpClientTimeoutSeconds:   GetEnvWithDefaultOrFail[int]("FESTWRAP_HTTP_CLIENT_TIMEOUT_S", 5),
 	}
 }
 
